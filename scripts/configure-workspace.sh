e358f9e979a75c50b74255f39b8ac4e88c827eda
Kevin Dang <kevindang22@gmail.com>
2022-07-19 19:11:26 -0400

    clean up config yabai script

--- scripts/configure-workspace.sh
+++ scripts/configure-workspace.sh
@@ -4,33 +4,46 @@ windows=$(yabai -m query --windows)
 
 windows_arr=$(echo "$windows" | jq -c '.[]')
 
-echo "$windows" | jq -c '.[]' | wc -l
-echo $windows_arr | wc -l
+#echo "$windows"
 
-d1_apps="^(Google Chrome)$"
+d1_apps=("Google Chrome")
 d1_space=1
-d2_apps="^(Alacritty|WebStorm)$"
+d2_apps=("Alacritty" "IntelliJ IDEA" "WebStorm" "CLion" "GoLand" "PyCharm")
 d2_space=2
-d3_apps="^(Spotify|Notion)$"
+d3_apps=("Spotify" "Notion")
 d3_space=3
+
+declare -A space_map
+
+function build_config() {
+  space_index="$1"
+  shift
+  apps=("${@}")
+
+  for app in "${apps[@]}"; do
+    space_map+=([$app]=$space_index)
+  done 
+}
+
+build_config $d1_space "${d1_apps[@]}"
+build_config $d2_space "${d2_apps[@]}"
+build_config $d3_space "${d3_apps[@]}"
+
+printf "SPACE_MAP: %s\n\n" "${space_map[@]@K}"
 
 i=0
 echo $windows | jq -c '.[]' | while read item; do
   app=$(jq -r '.app' <<< $item)
   window_id=$(jq -r '.id' <<< $item)
 
-  echo $i, $app, $window_id
-  echo ""
+  #echo $i, $app, $window_id
+  #echo ""
   let "i=++i"
 
-  if [[ $app =~ $d1_apps ]]; then
-    yabai -m window $window_id --space $d1_space
-    echo "MOVED CHROME"
-  elif [[ $app =~ $d2_apps ]]; then
-    yabai -m window $window_id --space $d2_space
-    echo "MOVED 2"
-  elif [[ $app =~ $d3_apps ]]; then
-    yabai -m window $window_id --space $d3_space
-    echo "MOVED 3"
+  if [[ -v space_map[$app] ]]; then
+    yabai -m window $window_id --space ${space_map[$app]}
+    echo "MOVED $app"
+  else
+    echo "NO ASSIGNED CONFIG for:" $app
   fi
 done
