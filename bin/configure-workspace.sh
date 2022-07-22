c3726de3189342f085945bf2b780c4598831d7db
Kevin Dang <kevindang22@gmail.com>
2022-07-22 17:51:23 -0400

    added config for multi monitor mode

--- bin/configure-workspace.sh
+++ bin/configure-workspace.sh
@@ -3,17 +3,31 @@ set -euo pipefail
 IFS=$'\n\t'
 
 windows=$(yabai -m query --windows)
+display_count=$(yabai -m query --displays | jq '. | length')
 
 windows_arr=$(echo "$windows" | jq -c '.[]')
 
 #echo "$windows"
+yabai -m space 1 --label main
+yabai -m space 2 --label code
+yabai -m space 3 --label chill
 
 d1_apps=("Google Chrome")
-d1_space=1
-d2_apps=("Alacritty" "IntelliJ IDEA" "WebStorm" "CLion" "GoLand" "PyCharm")
-d2_space=2
+d2_apps_1=("IntelliJ IDEA" "WebStorm" "CLion" "GoLand" "PyCharm")
+d2_apps_2=("Alacritty")
 d3_apps=("Spotify" "Notion")
-d3_space=3
+
+if [[ $display_count = 1 ]]; then
+  d1_space="--space main"
+  d2_space_1="--space code --grid 1:1:0:0:1:1"
+  d2_space_2="--space code"
+  d3_space="--space chill"
+else
+  d1_space="--space 1"
+  d2_space_1="--space 4"
+  d2_space_2="--space 3"
+  d3_space="--space 2"
+fi
 
 declare -A space_map
 
@@ -23,13 +37,14 @@ function build_config() {
   apps=("${@}")
 
   for app in "${apps[@]}"; do
-    space_map+=([$app]=$space_index)
+    space_map+=([$app]="$space_index")
   done 
 }
 
-build_config $d1_space "${d1_apps[@]}"
-build_config $d2_space "${d2_apps[@]}"
-build_config $d3_space "${d3_apps[@]}"
+build_config "$d1_space" "${d1_apps[@]}"
+build_config "$d2_space_1" "${d2_apps_1[@]}"
+build_config "$d2_space_2" "${d2_apps_2[@]}"
+build_config "$d3_space" "${d3_apps[@]}"
 
 printf "SPACE_MAP: %s\n\n" "${space_map[@]@K}"
 
@@ -43,9 +58,11 @@ echo $windows | jq -c '.[]' | while read item; do
   let "i=++i"
 
   if [[ -v space_map[$app] ]]; then
-    yabai -m window $window_id --space ${space_map[$app]}
-    echo "MOVED $app"
+    cmd="yabai -m window $window_id ${space_map[$app]}"
+    echo $cmd
+    eval $cmd
+    echo "MOVED $app - $window_id"
   else
-    echo "NO ASSIGNED CONFIG for:" $app
+    echo "NO ASSIGNED CONFIG for: $app - $window_id"
   fi
 done
