9083e4c9c3bb470fd8c3d51b8b5191677a4b538c
Kevin Dang <kevindang22@gmail.com>
2022-11-02 00:23:58 -0400

    minor vim diagnostic style

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -50,6 +50,22 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    float = {
+        border = "single",
+        format = function(diagnostic)
+            return string.format(
+                "%s (%s) [%s]",
+                diagnostic.message,
+                diagnostic.source,
+                diagnostic.code or diagnostic.user_data.lsp.code
+            )
+        end,
+    },
+})
+
 lspconfig.sumneko_lua.setup {
   on_attach = M.on_attach,
   capabilities = M.capabilities,
