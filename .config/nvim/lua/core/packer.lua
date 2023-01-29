f5a064a6c8bd3e4b12ff34d2c30779b09cb52ce0
Kevin Dang <kevindang22@gmail.com>
2023-01-29 16:18:30 -0500

    create packer snapshots

--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -4,6 +4,7 @@ M.options = {
   auto_clean = true,
   compile_on_sync = true,
   git = { clone_timeout = 6000 },
+  snapshot_path = "./packer_snapshots",
   display = {
     working_sym = "ﲊ",
     error_sym = "✗ ",
