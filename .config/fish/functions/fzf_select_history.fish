ae1f2ecbe6617babe9f9e4827d9076aab7eb73a7
Kevin Dang <kevindang22@gmail.com>
2023-02-11 14:27:33 -0500

    customize fzf hisotry

--- .config/fish/functions/fzf_select_history.fish
+++ .config/fish/functions/fzf_select_history.fish
@@ -1,15 +1,10 @@
 function fzf_select_history
-  if test (count $argv) = 0
-    set fzf_flags --layout=bottom-up
-  else
-    set fzf_flags --layout=bottom-up --query "$argv"
-  end
-
-  history|fzf $peco_flags|read foo
+  history | fzf --height 50% --reverse --border=rounded --border-label="Search History" --bind=change:top | read foo
 
   if [ $foo ]
     commandline $foo
   else
     commandline ''
   end
+  commandline -f repaint # needed so that the fish prompt is shown after fzf exits - https://github.com/fish-shell/fish-shell/issues/5945
 end
