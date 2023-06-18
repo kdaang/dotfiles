44e4f8e1a833c07e1bbff7b2f741a80c38ea725b
Kevin Dang <kevindang22@gmail.com>
2023-06-18 02:14:48 -0400

    update nnn plugs

--- .config/nnn/nnn-config.fish
+++ .config/nnn/nnn-config.fish
@@ -2,4 +2,4 @@ alias nnn='nnn -a'
 # for some reason $Z_DATA is not picked up by the autojump plugin so mapping to default z data location
 ln -s $Z_DATA $HOME/.z &> /dev/null
 
-export NNN_PLUG='p:preview-tui;z:autojump'
+export NNN_PLUG='g:preview-tui;z:autojump;f:fzcd'
