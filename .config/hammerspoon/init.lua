ceae2df74b7cdb5f25ec17a456508670a1e848d5
Kevin Dang <kevindang22@gmail.com>
2023-06-25 15:04:45 -0400

    refactor init lua

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,11 +1,14 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
+local hsHotkey = hs.hotkey
+local hsAlert = hs.hotkey
+local hsWindow = hs.hotkey
+
+hsHotkey.bind({"cmd", "alt", "ctrl"}, "W",
+              function() hsAlert.show("Hello World!") end)
+
+hsHotkey.bind({"cmd", "alt"}, "F", function()
+    local win = hsWindow.focusedWindow()
+    win:maximize(0)
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "F", function()
-  local win = hs.window.focusedWindow()
-  win:maximize(0)
-end)
-
-clipboard = require("clipboard")
-clipboard.setup()
+Clipboard = require("clipboard")
+Clipboard.setup()
