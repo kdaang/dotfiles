8df0764d18b2f809042840ca4a98a97ad4d1e8d2
Kevin Dang <kevindang22@gmail.com>
2023-06-25 18:13:17 -0400

    mute when off home network and unmute when on

--- .config/hammerspoon/wifi-actions.lua
+++ .config/hammerspoon/wifi-actions.lua
@@ -5,23 +5,43 @@ local hsScreen = require("hs.screen")
 WifiTransitions = require("lib.wifi-transitions")
 M = {}
 
-M.setup = function()
-    WifiTransitions.logger.setLogLevel(5)
-    WifiTransitions:start()
+local HOME_NETWORK_NAME = "BeyondFuture"
 
-    table.insert(WifiTransitions.actions, {
-        from = "BeyondFuture",
+local wifiActions = {
+    {
+        from = HOME_NETWORK_NAME,
         to = nil,
         fn = {
             function(event, interface, old_ssid, new_ssid)
                 hsAlert.show(string.format(
-                                 "Off Home Network - %s, saucing up...",
+                                 "Off Home Network - %s, cooling down...",
                                  new_ssid), hsScreen.allScreens(), 3)
 
                 hsAudioDevice.defaultOutputDevice():setOutputMuted(true)
             end
         }
-    })
+    }, {
+        from = nil,
+        to = HOME_NETWORK_NAME,
+        fn = {
+            function(event, interface, old_ssid, new_ssid)
+                hsAlert.show(string.format(
+                                 "Connected to Home Network - %s, spicing up...",
+                                 new_ssid), hsScreen.allScreens(), 3)
+
+                hsAudioDevice.defaultOutputDevice():setOutputMuted(false)
+            end
+        }
+    }
+}
+
+M.setup = function()
+    WifiTransitions.logger.setLogLevel(5)
+    WifiTransitions:start()
+
+    for _, wifiAction in ipairs(wifiActions) do
+        table.insert(WifiTransitions.actions, wifiAction)
+    end
 end
 
 return M
