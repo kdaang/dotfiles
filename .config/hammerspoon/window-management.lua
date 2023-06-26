64dc3d384c267f0b14711b4ee75608d470d90d35
Kevin Dang <kevindang22@gmail.com>
2023-06-26 00:09:40 -0400

    moved skhd f4 cw bind to hammerspoon with maximizing non managed windows

--- .config/hammerspoon/window-management.lua
+++ .config/hammerspoon/window-management.lua
@@ -13,6 +13,27 @@ M.setup = function()
         local win = hsWindow.focusedWindow()
         win:maximize(0)
     end)
+
+    hsHotkey.bind({}, "f4", function()
+        hs.execute("cw", true)
+
+        -- script returns list of unmanaged windows ids string in csv form
+        local output = hs.execute("yabaiw", true)
+        local unmanagedWindowIds = {}
+
+        -- split comma separated string
+        for unmanagedWindowId in (output .. ","):gmatch("(.-),") do
+            unmanagedWindowIds[tonumber(unmanagedWindowId)] = true
+        end
+
+        local unmanagedWindows = hsWindow.filter.new(function(window)
+            return unmanagedWindowIds[window:id()]
+        end):getWindows()
+
+        for _, unmanagedWindow in ipairs(unmanagedWindows) do
+            unmanagedWindow:maximize(0)
+        end
+    end)
 end
 
 return M
