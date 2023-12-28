4b6b3cbb2fcb8fd9ce8d2193d91fe40f1d0fe2a7
Kevin Dang <kevindang22@gmail.com>
2023-12-28 18:00:59 -0500

    update vim window resize mapping

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -68,10 +68,10 @@ M.general = {
         ["sj"] = {"<C-w>j", "move to bottom window"},
 
         -- resize windows
-        ["<C-w><left>"] = {"<C-w><", "shift left"},
-        ["<C-w><right>"] = {"<C-w>>", "shift right"},
-        ["<C-w><up>"] = {"<C-w>+", "shift up"},
-        ["<C-w><down>"] = {"<C-w>-", "shift bottom"},
+        ["<C-w>h"] = {"<C-w>10<", "shift left"},
+        ["<C-w>l"] = {"<C-w>10>", "shift right"},
+        ["<C-w>k"] = {"<C-w>10+", "shift up"},
+        ["<C-w>j"] = {"<C-w>10-", "shift bottom"},
 
         -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
         -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
