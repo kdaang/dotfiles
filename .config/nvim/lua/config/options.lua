0f20481498bff3cba57fa9717d9485202bd4eddd
Kevin Dang <kevindang22@gmail.com>
2023-12-29 22:12:00 -0500

    added lazyvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,54 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+local g = vim.g
+
+g.vim_version = vim.version().minor
+
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+
+opt.title = true
+opt.clipboard = "unnamedplus"
+opt.cul = true -- cursor line
+
+opt.wrap = false
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+opt.relativenumber = true
+
+-- disable nvim intro
+opt.shortmess:append("sI")
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append("<>[]hl")
+
+g.mapleader = " "
