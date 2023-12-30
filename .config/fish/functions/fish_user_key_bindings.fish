4595e7a853a1b95fef97cf264c67a2a6896438e7
Kevin Dang <kevindang22@gmail.com>
2023-12-30 18:37:20 -0500

    added fzf search fish bind

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,14 +1,17 @@
 function fish_user_key_bindings
-  # peco
-  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
-  # bind \cr fzf_select_history
+    # peco
+    bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+    # bind \cr fzf_select_history
 
-  bind \cf fzf_search_dir
+    bind \cf fzf_search_dir
 
-  # vim-like
-  bind \cl forward-char
-  fish_vi_key_bindings --no-erase insert
+    # vim-like
+    bind \cl forward-char
+    fish_vi_key_bindings --no-erase insert
 
-  # remove history
-  bind \cg forget_history
+    # remove history
+    bind \ch forget_history
+
+    # enable fzf to find and paste file
+    bind \cg _fzf_search_directory
 end
