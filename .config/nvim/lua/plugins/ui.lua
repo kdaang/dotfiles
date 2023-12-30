10b78f153102acf6a5eb31bfae6fe7238334f692
Kevin Dang <kevindang22@gmail.com>
2023-12-30 16:15:39 -0500

    edit bufferline

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+}
