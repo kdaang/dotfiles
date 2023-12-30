1a00fd4e4bdc3613135c72bd9c51cd5b3118b9c6
Kevin Dang <kevindang22@gmail.com>
2023-12-29 21:37:52 -0500

    rm nvchad nvim and init lazynvim

--- .config/nvim/lua/plugins/configs/auto-session.lua
+++
@@ -1,9 +0,0 @@
-local present, auto_session = pcall(require, "auto-session")
-
-if not present then return end
-
-local options = {}
-
-options = require("core.utils").load_override(options, "auto_session")
-
-auto_session.setup(options)
