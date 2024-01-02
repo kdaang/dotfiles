996dbeec237820f35c7391c63e843d7278a5f596
Kevin Dang <kevindang22@gmail.com>
2024-01-02 15:51:49 -0500

    set search scope and extension for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,4 +4,16 @@ local mappings = require("nivek.mappings")
 return {
   "nvim-telescope/telescope.nvim",
   keys = vim.list_extend(utils.getKeyMapping(mappings.telescope), { { "<leader>/", false } }),
+  opts = {
+    pickers = {
+      live_grep = {
+        mappings = {
+          i = {
+            ["<C-l>"] = require("nivek.custom.telescope").actions.set_folders,
+            ["<C-f>"] = require("nivek.custom.telescope").actions.set_extension,
+          },
+        },
+      },
+    },
+  },
 }
